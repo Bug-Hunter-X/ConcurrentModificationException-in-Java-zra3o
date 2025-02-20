e49fd0f4b9113aab7985547cc4c511f1219c0c40
BugHunter-X <askzenai@gmail.com>
2025-02-20 06:52:04 +0100

    Add ConcurrentModificationExceptionExample.java file

---
+++ ConcurrentModificationExceptionExample.java
@@ -0,0 +1,14 @@
+public class ConcurrentModificationExceptionExample {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        for (String item : list) {
+            if (item.equals("B")) {
+                list.remove(item);
+            }
+        }
+    }
+}
